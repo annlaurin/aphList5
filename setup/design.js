832477764f66c7822a71d93eb7c8ea6eff1c5259
Anna Laurinavichyute <annlaurin@gmail.com>
2015-04-15 21:31:58 +0300

    trying to change buttons

--- setup/design.js
+++ setup/design.js
@@ -6,11 +6,11 @@ Experigen.initialize = function () {
 	tests = items.pairWith("view","stimulus.ejs");
 
 	this.addStaticScreen("intro.ejs");
-//	this.addStaticScreen("warmup.ejs");
-//	this.addStaticScreen("warmup1.ejs");
-//	this.addStaticScreen("warmup2.ejs");
-//	this.addStaticScreen("warmup3.ejs");
-//	this.addStaticScreen("warmup4.ejs");
+	this.addStaticScreen("warmup.ejs");
+	this.addStaticScreen("warmup1.ejs");
+	this.addStaticScreen("warmup2.ejs");
+	this.addStaticScreen("warmup3.ejs");
+	this.addStaticScreen("warmup4.ejs");
 	this.addStaticScreen("getgoing.ejs")
 	this.addBlock(tests);
 	this.addStaticScreen("demographic.ejs");
